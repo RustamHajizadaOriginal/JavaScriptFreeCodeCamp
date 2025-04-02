6aceaf50f6b2731feaa9b7cfd8ac3fc845bddccf
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-04-02 10:04:42 +0200

    Else IF Statements

--- script.js
+++ script.js
@@ -267,7 +267,8 @@ testElseIf = (val) => {
     return "Greater than 10";
   } else if (val < 5) {
     return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
   }
-  return "Between 5 and 10";
 };
 console.log(testElseIf(4));
