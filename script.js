282fed9568df43e863583f3de3a156524918b4bd
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-04-02 08:59:01 +0200

    Boolean Values

--- script.js
+++ script.js
@@ -109,11 +109,17 @@
 // console.log(processed);
 
 // ========= Stand in Line ===============
-nextInLine = (arr, item) => {
-  arr.push(item);
-  return arr.shift();
-};
-const testArr = [1, 2, 3, 4, 5];
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
+// nextInLine = (arr, item) => {
+//   arr.push(item);
+//   return arr.shift();
+// };
+// const testArr = [1, 2, 3, 4, 5];
+// console.log("Before: " + JSON.stringify(testArr));
+// console.log(nextInLine(testArr, 6));
+// console.log("After: " + JSON.stringify(testArr));
+
+// ========= Boolean Values ===============
+
+// welcomeToBooleans = () => {
+//   return true;
+// };
