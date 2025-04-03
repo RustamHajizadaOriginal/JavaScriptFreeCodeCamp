0cba232995da6a4c8dc67597bfcd5a3bdc6b97aa
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-04-03 08:11:51 +0200

    Chaining Else IF Statements

--- script.js
+++ script.js
@@ -262,13 +262,29 @@
 // console.log(testElse(6));
 
 // ========= Else IF Statements ===============
-testElseIf = (val) => {
-  if (val > 10) {
-    return "Greater than 10";
-  } else if (val < 5) {
-    return "Smaller than 5";
-  } else {
-    return "Between 5 and 10";
+// testElseIf = (val) => {
+//   if (val > 10) {
+//     return "Greater than 10";
+//   } else if (val < 5) {
+//     return "Smaller than 5";
+//   } else {
+//     return "Between 5 and 10";
+//   }
+// };
+// console.log(testElseIf(4));
+
+// ========= Chaining Else IF Statements ===============
+testSize = (num) => {
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
   }
 };
-console.log(testElseIf(4));
+console.log(testSize(7));
