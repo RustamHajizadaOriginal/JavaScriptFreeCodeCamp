9bf576b81f114f5d5d1bcac2d2b3979130fc912b
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-04-03 08:35:02 +0200

    Switch Statements

--- script.js
+++ script.js
@@ -289,31 +289,52 @@
 // console.log(testSize(19));
 
 // ========= Else IF Statements Challenge ===============
-let names = [
-  "Hole-in-one!",
-  "Eagle",
-  "Birdie",
-  "Par",
-  "Bogey",
-  "Double Bogey",
-  "Go Home!",
-];
-goldScore = (par, strokes) => {
-  if (strokes == 1) {
-    return names[0];
-  } else if (strokes <= par - 2) {
-    return names[1];
-  } else if (strokes == par - 1) {
-    return names[2];
-  } else if (strokes == par) {
-    return names[3];
-  } else if (strokes == par + 1) {
-    return names[4];
-  } else if (strokes == par + 2) {
-    return names[5];
-  } else if (strokes >= par + 3) {
-    return names[6];
-  }
-};
+// let names = [
+//   "Hole-in-one!",
+//   "Eagle",
+//   "Birdie",
+//   "Par",
+//   "Bogey",
+//   "Double Bogey",
+//   "Go Home!",
+// ];
+// goldScore = (par, strokes) => {
+//   if (strokes == 1) {
+//     return names[0];
+//   } else if (strokes <= par - 2) {
+//     return names[1];
+//   } else if (strokes == par - 1) {
+//     return names[2];
+//   } else if (strokes == par) {
+//     return names[3];
+//   } else if (strokes == par + 1) {
+//     return names[4];
+//   } else if (strokes == par + 2) {
+//     return names[5];
+//   } else if (strokes >= par + 3) {
+//     return names[6];
+//   }
+// };
+// console.log(goldScore(5, 9));
 
-console.log(goldScore(5, 9));
+// ========= Switch Statements  ===============
+
+caseInSwitch = (val) => {
+  let answer = "";
+  switch (val) {
+    case 1:
+      answer = "Alpha";
+      break;
+    case 2:
+      answer = "Beta";
+      break;
+    case 3:
+      answer = "Gamma";
+      break;
+    case 4:
+      answer = "Delta";
+      break;
+  }
+  return answer;
+};
+console.log(caseInSwitch(1));
