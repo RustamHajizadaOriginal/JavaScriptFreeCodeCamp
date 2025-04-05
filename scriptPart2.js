67f4507c611cfa6013d2ab668b232f15828a5359
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-04-05 16:07:59 +0200

    Use the Rest Operator with Function Parameters

--- scriptPart2.js
+++ scriptPart2.js
@@ -219,3 +219,12 @@
 // })();
 // console.log(increment(5, 2)); // 7
 // console.log(increment(5)); // 6
+
+// ========= Use the Rest Operator with Function Parameters ===============
+const sum = (function () {
+  return function sum(x, y, z) {
+    const args = [x, y, z];
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
